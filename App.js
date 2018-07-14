a205db32513993ca3368f7f1dda347a2eb3ada0b
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-07-14 11:46:28 +0300

    reorganisation

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import MainScreen from './src/main-screen';
+import GameScreen from './src/game-screen';
 
 export default class App extends React.Component {
   render() {
-    return <MainScreen />;
+    return <GameScreen />;
   }
 }
